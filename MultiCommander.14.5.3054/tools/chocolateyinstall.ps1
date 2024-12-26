de14cd58261c70af7662c786d527b7dd136219ff
bibimagic <b23.martial@gmail.com>
2024-12-26 18:09:14 +0100

    Add files via upload

---
+++ MultiCommander.14.5.3054/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageArgs = @{
+	packageName    = 'multicommander'
+	fileType       = 'exe'
+	url            = "https://multicommander.com/files/updates/MultiCommander_win32_(14.5.0.3054).exe"
+	url64bit       = "https://multicommander.com/files/updates/MultiCommander_x64_(14.5.0.3054).exe"
+	softwareName   = 'MultiCommander'
+	checksum       = '385EE2D37B7F1352655F3CC253DF877BA4E599A4'
+	checksumType   = 'sha1'
+	checksum64     = '1C964B9D5E8EECC363F393415444B5155FA3520E'
+	checksumType64 = 'sha1'
+	silentArgs     = '/S'
+	validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
