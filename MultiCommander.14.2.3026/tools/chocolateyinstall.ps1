c4f0e86661dc744c9fa478c39de353568e6be932
bibimagic <b23.martial@gmail.com>
2024-12-26 18:06:42 +0100

    Add files via upload

---
+++ MultiCommander.14.2.3026/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageArgs = @{
+	packageName    = 'multicommander'
+	fileType       = 'exe'
+	url            = "https://multicommander.com/files/updates/MultiCommander_win32_(14.2.0.3026).exe"
+	url64bit       = "https://multicommander.com/files/updates/MultiCommander_x64_(14.2.0.3026).exe"
+	softwareName   = 'MultiCommander'
+	checksum       = '2B4FA655AE82FBC6ADE10BAF1CCDBC70FB45CC64'
+	checksumType   = 'sha1'
+	checksum64     = '2490B2B1B13F98AB4D3C12A427F61E4B8E7FACCB'
+	checksumType64 = 'sha1'
+	silentArgs     = '/S'
+	validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ MultiCommander.14.4.3047/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageArgs = @{
+	packageName    = 'multicommander'
+	fileType       = 'exe'
+	url            = "http://multicommander.com/files/updates/MultiCommander_win32_(14.4.0.3047).exe"
+	url64bit       = "http://multicommander.com/files/updates/MultiCommander_x64_(14.4.0.3047).exe"
+	softwareName   = 'MultiCommander'
+	checksum       = 'C9A9A4618B0ACF50B1BE442BEC9F736B9ACCDC46'
+	checksumType   = 'sha1'
+	checksum64     = 'FA63A56D0117CA64A4A69BF6CA9FDE5553F6ADD2'
+	checksumType64 = 'sha1'
+	silentArgs     = '/S'
+	validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
