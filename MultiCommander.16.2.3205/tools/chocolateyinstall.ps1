cc5121c40d5ef3bcaae1c9cf823798a6e8aea040
bibimagic <bibimagic@disroot.org>
2026-08-28 18:21:09 +0200

    Add files via upload

---
+++ MultiCommander.16.2.3205/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageArgs = @{
+	packageName    = 'multicommander'
+	fileType       = 'exe'
+	url            = "https://multicommander.com/files/updates/MultiCommander_win32_(16.2.0.3205).exe"
+	url64bit       = "https://multicommander.com/files/updates/MultiCommander_x64_(16.2.0.3205).exe"
+	softwareName   = 'MultiCommander'
+	checksum       = '85F9CD3A2EE3BA2042FB00F5BD46D47E704C8485'
+	checksumType   = 'sha1'
+	checksum64     = 'B30B89105DF8C9154284CAA371310CC302295277'
+	checksumType64 = 'sha1'
+	silentArgs     = '/S'
+	validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ MultiCommander.16.2.3210/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageArgs = @{
+	packageName    = 'multicommander'
+	fileType       = 'exe'
+	url            = "https://multicommander.com/files/updates/MultiCommander_win32_(16.2.0.3210).exe"
+	url64bit       = "https://multicommander.com/files/updates/MultiCommander_x64_(16.2.0.3210).exe"
+	softwareName   = 'MultiCommander'
+	checksum       = '2374FBB99A06BC8170AA8BFAD399439BBB0CF29E'
+	checksumType   = 'sha1'
+	checksum64     = '3083CE31BE8D4DD40A9A19DF08C9E645744E1AF4'
+	checksumType64 = 'sha1'
+	silentArgs     = '/S'
+	validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
